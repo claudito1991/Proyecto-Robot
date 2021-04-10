f25eb798cddd28db9268bd99780d44df01a0612c
WhiteAlucard <claudio.oulego@gmail.com>
2021-04-10 08:46:07 -0300

    last modifications

---
+++ Assets/Scripts/changeColor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Collision");
+            GetComponent<MeshRenderer>().material.color = Color.magenta;
+            
+        }
+    }
+}
